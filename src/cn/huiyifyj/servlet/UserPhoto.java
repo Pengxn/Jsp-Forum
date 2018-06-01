96e32e84ca2ced7b7f703cc52f43cf37affccb94
HuiyiFYJ <jxfengyijie@gmail.com>
2018-06-01 13:06:17 +0800

    Create UserPhoto servlet

---
+++ src/cn/huiyifyj/servlet/UserPhoto.java
@@ -0,0 +1,34 @@
+package cn.huiyifyj.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserPhoto")
+public class UserPhoto extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		doGet(req, resp);
+		
+	}
+
+}
