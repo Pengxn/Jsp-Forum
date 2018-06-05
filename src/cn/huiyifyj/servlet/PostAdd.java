426da8a5ba1ec87d48a745da1aa4f0cbad8ae816
HuiyiFYJ <jxfengyijie@gmail.com>
2018-06-05 18:34:33 +0800

    Update  and optimize Servlet PostAdd
    更新 PostAdd 的 Servlet, 进行优化。

--- src/cn/huiyifyj/servlet/PostAdd.java
+++ src/cn/huiyifyj/servlet/PostAdd.java
@@ -1,10 +1,6 @@
 package cn.huiyifyj.servlet;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import cn.huiyifyj.bean.Post;
 import cn.huiyifyj.bean.User;
@@ -31,123 +22,42 @@ public class PostAdd extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		req.setCharacterEncoding("UTF-8");
-		
-		resp.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html; charset=UTF-8");
-		
-		// 存放构造的文件（图片）名
-		File fullFile = null;
-		
-		// 存放标题和内容
-		String titles = null;
-		String pContent = null;
-		
-		HttpSession session = req.getSession();
-		
-		User user = (User) session.getAttribute("user");
-		
-		int userId = user.getUserId();
-		String userName = user.getName();
-		
-		Post post = new Post();
-		
-		/**
-		 * 将头像上传保存到相应目录
-		 * 
-		 */
-		// 获取当前项目目录
-		String realPath = getServletContext().getRealPath("/");
-		
-		realPath = realPath.substring(0, realPath.indexOf("\\.")).replace("\\", "/");
-		
-		String contextPath = req.getContextPath().replace("\\", "/");
-		
-		System.out.println(realPath + contextPath);
-		
-		String uploadPath = (realPath + contextPath) + "/WebRoot/uploda_Post";
-		
-		boolean isMultipart = ServletFileUpload.isMultipartContent(req);
-		
-		if (!isMultipart) {
-			System.out.println("enctype必须是 multipart/form-data");
-		}
-		
-		try {
-			
-			FileItemFactory factory = new DiskFileItemFactory();
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			
-			List<FileItem> items = upload.parseRequest(req);// 得到所有的文件
-			
-			Iterator<FileItem> itr = items.iterator();
 
-			// 改写格式
-			Date date = new Date();
-			
-			String time = date.toLocaleString();
-			time = time.replace(" ", "_");
-			time = time.replace("-", "_");
-			time = time.replace(":", "_");
+		doPost(req, resp);
 
-			// 依次处理每个文件
-			while (itr.hasNext()) {
-
-				FileItem item = (FileItem) itr.next();
-				
-				String fileName = item.getName(); // 获得文件名，包括路径
-				
-				if (fileName != null) {
-
-					fullFile = new File(time + "_id" + userId + "_" + item.getName());
-					
-					File savedFile = new File(uploadPath, fullFile.getName());
-					
-					item.write(savedFile);
-					
-				}
-				
-				// 检查是一个普通的表单域还是File组件（true是普通表单）
-				if (item.isFormField()) {
-					
-					// 检查表单属性
-					if ("titles".equals(item.getFieldName())) {
-						titles = new String(item.getString().getBytes("iso-8859-1"), "UTF-8");
-					}
-					
-					if ("fcontent".equals(item.getFieldName())) {
-						pContent = new String(item.getString().getBytes("iso-8859-1"), "UTF-8");
-					}
-					
-				}
-				
-			}
-
-			System.out.println("上传成功");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		// 将发帖的图片信息插入数据库
-		post.setTitles(titles);
-		post.setpContent(pContent);
-		post.setUserId(userId);
-		post.setPhoto(fullFile.getName());
-		post.setUserName(userName);
-		
-		PostConnect.save(post);
-		
-		resp.sendRedirect("myself/mycenter.jsp");
-		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		doGet(req, resp);
+		req.setCharacterEncoding("UTF-8");
+		
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		// 存放标题和内容
+		String title = req.getParameter("title");
+		String pContent = req.getParameter("pContent");;
+		
+		HttpSession session = req.getSession();
+		
+		User user = (User) session.getAttribute("loginer");
+		
+		int userId = user.getUserId();
+		String userName = user.getName();
+		
+		Post post = new Post();
+		
+		// 将发帖的图片信息插入数据库
+		post.setTitles(title);
+		post.setpContent(pContent);
+		post.setUserId(userId);
+		post.setUserName(userName);
+		
+		PostConnect.save(post);
+		
+		resp.sendRedirect("index.jsp");	// 	其实更好的应该是去跳向帖子内容页面
 		
 	}
 
